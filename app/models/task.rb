5bd9ddfe77f8fd8ebb01e540ecc012039550f872
Sean Dela Cruz <andreadlacrz1@gmail.comm>
2022-03-03 20:35:47 +0800

    update

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,15 @@ class Task < ApplicationRecord
         validates :category, presence: true
         validates :task_title, presence: true
         validates :notes, length: {maximum: 150}
+
+
+        config.authentication_keys = [ :email ]
+        config.case_insensitive_keys = [ :email ]
+        config.strip_whitespace_keys = [ :email ]
+
+
+        devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
+      
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   resources :categories
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations'}
   resources :tasks
-  
+  resources :users
+
+
   get "edit/category/:id/" => "categories#edit"
+
+  get 'signup'  => 'users#new'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  
   
   get 'home/about'
   root 'categories#index' 
